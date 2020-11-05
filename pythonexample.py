98ef5948a5f93f8941d70644b557333461fabbfd
yandachu <yandachu@umich.edu>
2020-11-05 14:43:30 -0500

    Add to example

--- pythonexample.py
+++ pythonexample.py
@@ -8,7 +8,8 @@ def get_names_array():
 
 
 # Write a program that prints the 5 most popular firstnames and the 5 most popular last names from the names array
-# hint: the str.split function allows for easy string splitting
+# hint: the str.split() function allows for easy string splitting
+# hint: use dictionaries to count names
 # hint: to sort a dict largest value first, try sorted_list = sorted(first_names.items(), key=lambda x: x[1], reverse=True)
 def main():
     names = get_names_array()
