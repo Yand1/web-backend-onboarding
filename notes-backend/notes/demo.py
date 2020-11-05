14d3b692f9a01c493969afb2968f101e256818d3
yandachu <yandachu@umich.edu>
2020-11-05 18:56:33 -0500

    Added demo/names

--- notes-backend/notes/demo.py
+++ notes-backend/notes/demo.py
@@ -1,4 +1,5 @@
 import flask
+import os
 import notes
 
 # Route: /demo/
@@ -56,4 +57,56 @@ def path(note):
 #            modify the corresponding note
 @notes.app.route('/demo/body')
 def body():
-    return 'Hello, World!'
+    body = flask.request.get_json()
+    return body["text"] + "\nP.S." + body["additional"]
+
+# Route: /demo/names
+# Objective: Take in a file, and perform the demo operations
+@notes.app.route('/demo/names')
+def names():
+    names = []
+    with open(os.getcwd() + "/names.txt", "r") as f:
+        for line in f:
+            names.append(line)
+
+    firstnames = {}
+    lastnames = {}
+    
+    for name in names:
+        name_arr = name.split()
+
+        if name_arr[0] not in firstnames:
+            firstnames[name_arr[0]] = 0
+
+        firstnames[name_arr[0]] += 1
+
+        if name_arr[1] not in lastnames:
+            lastnames[name_arr[1]] = 0
+
+        lastnames[name_arr[1]] += 1
+
+    output_str = """
+    <!DOCTYPE html>
+    <html>
+        <body>
+    """
+
+    firstnames_sorted_list = sorted(firstnames.items(), key=lambda x: x[1], reverse=True)
+    output_str += "Firstnames:<ul>"
+    for i in range(5):
+        output_str += "<li>" + firstnames_sorted_list[i][0] + ", count = " + str(firstnames_sorted_list[i][1]) + "<\li>"
+        
+    output_str += "</ul>"
+
+    lastnames_sorted_list = sorted(lastnames.items(), key=lambda x: x[1], reverse=True)
+    output_str += "Lastnames:<ul>"
+    for i in range(5):
+        output_str += "<li>" +lastnames_sorted_list[i][0] + ", count = " + str(lastnames_sorted_list[i][1]) + "<\li>"
+
+    output_str += """
+        </ul>
+        </body>
+    </html>
+    """
+    return output_str
+    
