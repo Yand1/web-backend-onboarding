76101890e57d771920a507f8603be717a309da52
yandachu <yandachu@umich.edu>
2020-11-12 16:32:41 -0500

    Updated demo

--- notes-backend/notes/demo.py
+++ notes-backend/notes/demo.py
@@ -30,15 +30,20 @@ def html():
     
     return ret
 
-# Route: /demo/sql
-# Objective: Execute a sql command on the database'
-@notes.app.route('/demo/sql')
-def sql():
-    return 'Hello, World!'
+# Route: /demo/json
+# Objective: Return some JSON
+@notes.app.route('/demo/json')
+def json_endpt():
+    count_dict = {}
+    
+    for i in range(3):
+        count_dict["element" + str(i)] = i
+
+    return count_dict, 200
 
 # Route: /demo/param
-# Objective: Take in a query parameter id, and use 
-#            it to modify the corresponding note
+# Objective: Take in a query parameter, and use 
+#            it to print a note
 @notes.app.route('/demo/param/')
 def param():
     note = flask.request.args.get('note')
@@ -47,7 +52,7 @@ def param():
 
 # Route: /demo/path
 # Objective: Take in a path parameter, and use it to
-#            modify the corresponding note
+#            print the  note
 @notes.app.route('/demo/path/<note>')
 def path(note):
     return "You sent the note: " + str(note)
@@ -58,14 +63,14 @@ def path(note):
 @notes.app.route('/demo/body')
 def body():
     body = flask.request.get_json()
-    return body["text"] + "\nP.S." + body["additional"]
+    return body["text"] + "\nP.S. " + body["additional"]
 
 # Route: /demo/names
-# Objective: Take in a file, and perform the demo operations
-@notes.app.route('/demo/names')
-def names():
+# Objective: Take in a file, and run the example program on that file
+@notes.app.route('/demo/names/<filename>')
+def names(filename):
     names = []
-    with open(os.getcwd() + "/names.txt", "r") as f:
+    with open(os.getcwd() + "/" + filename, "r") as f:
         for line in f:
             names.append(line)
 
@@ -109,4 +114,96 @@ def names():
     </html>
     """
     return output_str
+
+# Route: /demo/names/body
+# Objective: Take in a json body of first names, and return the most common name
+# Input: Json as such:
+# {
+#     "firstnames": ["name1", "name2, "name3"]
+# }
+# Output: Json as such:
+# {
+#     "name": "most_common_name"
+#     "count": "number_of_appearances"
+# }
+@notes.app.route('/demo/names/body')
+def names_body():
+    firstnames = flask.request.get_json()["firstnames"]
+    firstnames_count = {}
+    for name in firstnames:
+        if name not in firstnames_count:
+            firstnames_count[name] = 0
+
+        firstnames_count[name] += 1
     
+    max_name = max(firstnames_count.items(), key=lambda k: k[1])
+
+    return {
+        "name": max_name[0],
+        "count": max_name[1]
+    }
+
+storage = {
+}
+
+# Route: /demo/storage
+# GET:
+# Objective: Take in a key, and return that element from storage
+#            return an error message and 400 bad request if key does not exist
+# Output:
+# {
+#     "key": "key",
+#     "value": "val"
+# }
+#
+# DELETE:
+# Objective: Take in a key, and delete that element from storage
+@notes.app.route('/demo/storage/<key>', methods=["GET", "DELETE"])
+def get_or_delete(key):
+    if flask.request.method == "DELETE":
+        storage.pop(key, None)
+
+        return '', 204
+    elif flask.request.method == "GET":
+        if key not in storage:
+            return {"message": "key doesn't exist"}, 400
+
+        return {
+            "key": key,
+            "value": storage[key]
+        }
+
+# Route: /demo/storage
+# POST:
+# Objective: Take in a json body of key and value, and store it in storage
+# Input:
+# {
+#     "key": "x",
+#     "value": 1
+# }
+# Output: return the input json, and 201 Created on success,
+#         return an error message and 400 bad request if key already exists
+#
+# PUT: 
+# Objective: Take in a json body of key and value, and modify the key in storage
+# Input:
+# {
+#     "key": "x",
+#     "value": 1
+# }
+# Output: return the input json, and 201 Created on success,
+#         return an error message and 400 bad request if key does not exist
+@notes.app.route('/demo/storage', methods=["POST", "PUT"])
+def post_or_put():
+    original_json = flask.request.get_json()
+    key = original_json["key"]
+    value = original_json["value"]
+
+    if flask.request.method == "POST" and key in storage:
+        return {"message": "key already exists"}, 400
+    elif flask.request.method == "PUT" and key not in storage:
+        return {"message": "key doesn't exist"}, 400
+
+    storage[key] = value
+
+    return original_json, 201
